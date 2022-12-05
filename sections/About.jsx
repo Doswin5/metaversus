e478b7180fbc0be0cdb5b2990a193b105bd3addb
DOSWIN <ayomidedosunmuoreoluwa@gmail.com>
2022-12-05 16:49:35 +0100

    Navbar and Hero Section

---
+++ sections/About.jsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { motion } from "framer-motion";
+import { TypingText } from '../components'
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+
+    </motion.div>
+    About section
+  </section>
+);
+
+export default About;
